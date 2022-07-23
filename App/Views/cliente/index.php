4b8e037b85500d3ad4aa8496718905e4258bb783
Victor V <victorverd@hotmail.com>
2022-07-23 11:54:20 -0300

    alterações no form

--- App/Views/cliente/index.php
+++ App/Views/cliente/index.php
@@ -70,42 +70,42 @@
             <div class="modal-body">
                 <form action="<?= url('cliente') ?>" id="formInclusao" method="POST">
                     <div id="mensagem_erro" name="mensagem_erro"></div>
-                    <input type="hidden" id="CSRF_token" name="CSRF_token" value="" />
+                    <input type="hidden" id="CSRF_token" name="CSRF_token" value="" required/>
                     <div class="form-group">
                         <label for="nome">Nome*</label>
-                        <input type="text" class="form-control" id="nome" name="nome">
+                        <input type="text" class="form-control" id="nome" name="nome" maxlength="50" required>
                     </div>
                     <div class="form-group">
                         <label for="cpf">CPF*</label>
-                        <input type="text" class="form-control" id="cpf" name="cpf">
+                        <input type="text" class="form-control" id="cpf" name="cpf" maxlength="14" required>
                     </div>
                     <div class="form-group">
                         <label for="endereco">Endereço*</label>
-                        <input type="text" class="form-control" id="endereco" name="endereco">
+                        <input type="text" class="form-control" id="endereco" name="endereco" maxlength="50" required>
                     </div>
                     <div class="form-group">
                         <label for="bairro">Bairro*</label>
-                        <input type="text" class="form-control" id="bairro" name="bairro">
+                        <input type="text" class="form-control" id="bairro" name="bairro" maxlength="50" required>
                     </div>
                     <div class="form-group">
                         <label for="cidade">Cidade*</label>
-                        <input type="text" class="form-control" id="cidade" name="cidade">
+                        <input type="text" class="form-control" id="cidade" name="cidade" maxlength="50" required>
                     </div>
                     <div class="form-group">
                         <label for="uf">UF*</label>
-                        <input type="text" class="form-control" id="uf" name="uf">
+                        <input type="text" class="form-control" id="uf" name="uf" maxlength="2" required>
                     </div>
                     <div class="form-group">
                         <label for="cep">CEP*</label>
-                        <input type="text" class="form-control" id="cep" name="cep">
+                        <input type="text" class="form-control" id="cep" name="cep" maxlength="8" required>
                     </div>
                     <div class="form-group">
                         <label for="telefone">Telefone*</label>
-                        <input type="tel" class="form-control" id="telefone" name="telefone">
+                        <input type="tel" class="form-control" id="telefone" name="telefone" maxlength="20" required>
                     </div>
                     <div class="form-group">
                         <label for="email">E-mail*</label>
-                        <input type="email" class="form-control" id="email" name="email">
+                        <input type="email" class="form-control" id="email" name="email" maxlength="50" required>
                     </div>
                 </form>
             </div>
@@ -133,44 +133,44 @@
 
                     <div id="mensagem_erro_alteracao" name="mensagem_erro_alteracao"></div>
 
-                    <input type="hidden" id="CSRF_token" name="CSRF_token" value="" />
-                    <input type="hidden" id="id_alteracao" name="id_alteracao" value="" />
+                    <input type="hidden" id="CSRF_token" name="CSRF_token" value="" required/>
+                    <input type="hidden" id="id_alteracao" name="id_alteracao" value="" required/>
 
                     <div class="form-group">
                         <label for="nome_alteracao">Nome*</label>
-                        <input type="text" class="form-control" id="nome_alteracao" name="nome_alteracao">
+                        <input type="text" class="form-control" id="nome_alteracao" name="nome_alteracao" maxlength="50" required>
                     </div>
                     <div class="form-group">
                         <label for="cpf_alteracao">CPF*</label>
-                        <input type="text" class="form-control" id="cpf_alteracao" name="cpf_alteracao">
+                        <input type="text" class="form-control" id="cpf_alteracao" name="cpf_alteracao" maxlength="14" required>
                     </div>
                     <div class="form-group">
                         <label for="endereco_alteracao">Endereço*</label>
-                        <input type="text" class="form-control" id="endereco_alteracao" name="endereco_alteracao">
+                        <input type="text" class="form-control" id="endereco_alteracao" name="endereco_alteracao" maxlength="50" required>
                     </div>
                     <div class="form-group">
                         <label for="bairro_alteracao">Bairro*</label>
-                        <input type="text" class="form-control" id="bairro_alteracao" name="bairro_alteracao">
+                        <input type="text" class="form-control" id="bairro_alteracao" name="bairro_alteracao" maxlength="50" required>
                     </div>
                     <div class="form-group">
                         <label for="cidade_alteracao">Cidade*</label>
-                        <input type="text" class="form-control" id="cidade_alteracao" name="cidade_alteracao">
+                        <input type="text" class="form-control" id="cidade_alteracao" name="cidade_alteracao" maxlength="50" required>
                     </div>
                     <div class="form-group">
                         <label for="uf_alteracao">UF*</label>
-                        <input type="text" class="form-control" id="uf_alteracao" name="uf_alteracao">
+                        <input type="text" class="form-control" id="uf_alteracao" name="uf_alteracao" maxlength="2" required>
                     </div>
                     <div class="form-group">
                         <label for="cep_alteracao">CEP*</label>
-                        <input type="text" class="form-control" id="cep_alteracao" name="cep_alteracao">
+                        <input type="text" class="form-control" id="cep_alteracao" name="cep_alteracao" maxlength="8" required>
                     </div>
                     <div class="form-group">
                         <label for="telefone_alteracao">Telefone*</label>
-                        <input type="tel" class="form-control" id="telefone_alteracao" name="telefone_alteracao">
+                        <input type="tel" class="form-control" id="telefone_alteracao" name="telefone_alteracao" maxlength="20" required>
                     </div>
                     <div class="form-group">
                         <label for="email_alteracao">E-mail*</label>
-                        <input type="email" class="form-control" id="email_alteracao" name="email_alteracao">
+                        <input type="email" class="form-control" id="email_alteracao" name="email_alteracao" maxlength="50" required>
                     </div>
                 </form>
             </div>
